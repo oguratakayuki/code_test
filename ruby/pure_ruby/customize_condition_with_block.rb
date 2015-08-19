5a5af8ca514bde4c7d1fbb53b54cdf582b0e1a88
oguratakayuki <otn.ogura@gmail.com>
2015-08-19 17:52:41 +0900

    blockで設定を渡す

--- ruby/pure_ruby/customize_condition_with_block.rb
+++ ruby/pure_ruby/customize_condition_with_block.rb
@@ -1,10 +1,24 @@
 class Event
   def initialize
-    @conf = Configuration.new
+    @configurations ={}
   end
-  def configuration
-    yield(@conf) if block_given?
+  #def configuration(case)
+  #  conf = Configuration.new
+  #  yield(conf) if block_given?
+  #  @configurations[case] = conf
+  #end
+
+  def configuration(on, &block)
+    @configurations[on] = block
   end
+
+  def execute(on)
+    conf = Configuration.new
+    @configurations[on].call(conf)
+    puts "type=#{conf.type}, place=#{conf.place}, time=#{conf.time}"
+  end
+
+
   class Configuration
     attr_accessor :place, :type, :time
     def initialize
@@ -16,11 +30,24 @@ class Event
 end
 
 event = Event.new
-event.configuration do |c|
-  c.place = :tokyo
+event.configuration(:morning) do |c|
   c.type = :all_day
+  c.place = :tokyo
   c.time = nil
 end
+
+event.configuration(:night) do |c|
+  c.type = :half_hour
+  c.place = :osaka
+  c.time = nil
+end
+
+event.execute(:morning)
+event.execute(:night)
+
+
+
 #eventの中のconfには直接アクセスさせない.
 # x event.conf.place = :hoge
-#blockで設定を渡すことで
+# 上記の用に書くより設定ファイルの記述に近く見通しが良い
+#blockで設定を渡すことで実行するタイミングを制御できる
