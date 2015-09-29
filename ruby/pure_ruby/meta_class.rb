c37bbc1eeecc12a81fbabea184f3bb88a6c08224
oguratakayuki <otn.ogura@gmail.com>
2015-09-29 18:35:58 +0900

    metaクラスはsingletonクラス?

--- ruby/pure_ruby/meta_class.rb
+++ ruby/pure_ruby/meta_class.rb
@@ -6,6 +6,8 @@ end
 
 hoge = Hoge.new
 
+#Ruby provides a syntax for accessing an object's metaclass directly. By doing class << Person
+#=無名クラス、Singletonクラス
 class << hoge
   def fuga
     puts 'fuga'
@@ -21,3 +23,17 @@ puts metaclass.instance_methods.grep(/fuga/)
 
 metaclass = class << hoge; self; end
 puts metaclass.instance_methods.grep(/fuga/)
+
+# object in Ruby also has its own metaclass
+piyo = Object.new
+def piyo.piyo1
+  "piyo1"
+end
+
+class << piyo
+  def piyo2
+    puts 'piyo2'
+  end
+end
+metaclass = class << piyo; self; end
+puts metaclass.instance_methods.grep(/piyo/)
