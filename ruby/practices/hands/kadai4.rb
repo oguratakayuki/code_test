422db1d9dbb8b55dbcf6a4c7c0373cf2836b6b56
oguratakayuki <otn.ogura@gmail.com>
2016-09-22 15:54:50 +0900

    余計なデバッグ処理を削除

--- ruby/practices/hands/kadai4.rb
+++ ruby/practices/hands/kadai4.rb
@@ -3,17 +3,19 @@ require 'active_model'
 require 'uri'
 class Investigator
   include ActiveModel::Model
-  attr_accessor :search_word
+  attr_accessor :search_word, :successful
   def initialize *args
     @agent = Mechanize.new
+    @investigated = false
     super *args
   end
-  def execute target_url
+  def find_text target_url
     @domain = target_url
     @agent.get(target_url)
-    @agent.page.search('body').text.match(/#{@search_word}/)
+    @successful = @agent.page.search('body').text.match(/#{@search_word}/)
   end
-  def links
+  def executable_links
+    #access可能なリンク
     @agent.page.search("body").css("a").reject{|link| link['href'] =~ /javascript/  || link['href'] =~ /mailto/ }.map{|link| URI.join(@domain, link['href']).to_s.chomp('/') }
   end
 end
@@ -21,50 +23,43 @@ class CrawlingLogger
 end
 class CrawlingRuler
   attr_reader :results
-  def initialize(domain, word, investigator)
+  #crawlingする時の動きを制御
+  #各ページで何をするかはinvestigatorに任せる
+  def initialize(domain, investigator)
     @investigator = investigator
     @domain = domain
     @results = []
     @visited = []
-    @total_loop_count_for_debug = 0
-    shortest_url_path_word_exist(@domain, word, consumed_time=0, histories=[])
   end
   def valid_url link
     URI.parse(link).host == URI.parse(@domain).host
   end
-  def shortest_url_path_word_exist(target_url, search_word, consumed_time=0, histories=[])
-    @total_loop_count_for_debug = @total_loop_count_for_debug + 1
-    return false if @total_loop_count_for_debug > 15
-    if histories.size > 2
-      puts 'visited'
-      return false
-    end
+  def crawl(target_url, search_word, consumed_time=0, histories=[])
     start_time = Time.now
-    agent = Mechanize.new
     sleep 3
     histories = histories << target_url
     @visited = @visited << target_url
     #あればtrue ここではあったらそのページにあるリンクは探索しない
-    
-    if @investigator.execute(target_url)
+    @investigator.find_text(target_url)
+    if @investigator.successful
       puts 'success'
       end_time = Time.now
       consumed_time = consumed_time + (end_time - start_time)
       @results << {consumed_time: consumed_time, histories: histories}
     else
-      @investigator.links.reject{|link_url| @visited.include?(link_url) }.each do |link|
+      @investigator.executable_links.reject{|link_url| histories.include?(link_url) }.each do |link|
         end_time = Time.now
         consumed_time = consumed_time + (end_time - start_time)
-        shortest_url_path_word_exist(link.to_s, search_word, consumed_time, histories) if valid_url(link.to_s)
+        crawl(link.to_s, search_word, consumed_time, histories) if valid_url(link.to_s)
       end
     end
   end
 end
-
-target_url = 'https://www.menriku.com'
-search_word = '男子スタッフを募集しておりますので'
-search_word = '兵庫'
-crawling_ruler = CrawlingRuler.new(target_url, search_word, Investigator.new(search_word: search_word))
-puts 'END!!!!'
+#target_urlに対象のURLを指定
+#sesarch_wordに検索文字列を指定
+target_url = 'https://www.example.com'
+search_word = 'aaa'
+crawling_ruler = CrawlingRuler.new(target_url, Investigator.new(search_word: search_word))
+crawling_ruler.crawl(target_url, search_word, consumed_time=0, histories=[])
 puts crawling_ruler.results.sort{|a,b| a[:consumed_time] <=> b[:consumed_time] }.first
 puts crawling_ruler.results.size
